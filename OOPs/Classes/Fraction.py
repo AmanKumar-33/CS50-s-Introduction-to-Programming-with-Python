79e2c832ca656b197681b07e0e9e30a904330610
Aman Kumar <89511864+AmanKumar-33@users.noreply.github.com>
2024-09-29 07:29:10 +0530

    Add files via upload
    Here I have written about OOPs basic class and doddle is given

---
+++ OOPs/Classes/Fraction.py
@@ -0,0 +1,32 @@
+class SimpleFraction(object):
+    
+    def __init__(self,num,deno):
+        self.num = num
+        self.deno = deno
+
+    def get_reverse(self):
+        """Return a float representing 1/self    """
+        # write your code here
+        return self.denom/self.num 
+        
+
+ 
+   
+    def invert(self):
+        """set the self's numerator to its denominatior and vice versa"""
+            #   write your code here
+            # 
+            #  
+        # (self.num , self.deno) = (self.deno,self.num)
+        """Other ways to the same thing is"""
+        newnum = self.deno
+        newdeno = self.num 
+        self.num = newnum
+        self.deno = newdeno
+        
+
+f1 = SimpleFraction(3,4)
+print(f1.num , f1.deno)
+# print(f1.get_reverse)
+f1.invert()
+print(f1.num , f1.deno) 
